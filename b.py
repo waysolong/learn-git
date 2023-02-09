9e349f4ec4092cd60f5b6d6a9af23224c37ef07e
wuxulong <xulong.wu>
2023-02-09 05:39:31 +0000

    b

---
+++ b.py
@@ -0,0 +1,10 @@
+'''
+Author: Wxl
+Date: 2023-02-09 05:36:32
+LastEditors: wuxulong xulong.wu
+LastEditTime: 2023-02-09 05:38:16
+FilePath: /workspace/learn-git/b.py
+Description: 
+Copyright (c) 2023 by ${git_name} email: ${git_email}, All Rights Reserved.
+'''
+import os
